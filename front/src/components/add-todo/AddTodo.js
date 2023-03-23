1d817c0beca713420847f0f9b1246c5b15bb252a
stephen-nene <stephen.nene@student.moringaschool.com>
2023-03-23 15:00:59 +0300

    adding a todo done

--- front/src/components/add-todo/AddTodo.js
+++ front/src/components/add-todo/AddTodo.js
@@ -2,11 +2,27 @@ import React from "react";
 
 function AddTodo(){
 
+    const addTodo = () => {
+        // Implement your add-todo logic here
+        console.log("todo added");
+      };
+
 
     return (
-        <div>
-            <h1>Add Todo</h1>
-        </div>
+ <div className="main" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+     <h1>Add Todo</h1>
+         <div className="addtodo">
+             <div className="mb-3">
+                 <label className="form-label">Title</label>
+                 <input type="text" className="form-control" placeholder="To-Do title" />
+             </div>
+             <div className="mb-3">
+                 <label  className="form-label">Summary</label>
+                 <textarea className="form-control" rows="3"></textarea>
+             </div>
+         </div>
+         <button className="btn btn-outline-success" onClick={addTodo}>Add Todo</button>
+ </div>
     )
 }
 
--- front/src/components/profile/Profile.js
+++ front/src/components/profile/Profile.js
@@ -1,19 +1,34 @@
 import React from "react";
+import buggati from "../../images/bugatti4.webp";
 
-function AddTodo(){
+function Profile() {
+  const handleLogout = () => {
+    // Implement your logout logic here
+    console.log("User logged out");
+  };
 
-
-    return (
-        <div>
-            <h1>Profiles</h1>
-            <div className="card" style ={{width : '18rem'}}>
-                <img src="www.bugatti.com/chiron-sport/" alt="..." className="card-img-top" />
-                <div className="card-body">
-                    <h5 className="card-title">steve-nene</h5>
-                </div>
-            </div>
+  return (
+    <div className="main"
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <h1>Profile</h1>
+      <div className="card" style={{ width: "25rem" }}>
+        <img src={buggati} alt="..." className="card-img-top" />
+        <div className="card-body">
+          <h3 className="card-title">steve-nene</h3>
+          <h6 className="text">Stevekid705@gmail.com</h6>
+          <p className="card-text">This is a buggati and dont worry much</p>
+          <button
+            type="button"
+            className="btn btn-outline-danger"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default AddTodo;
+export default Profile;
