7327daaa704e1fb7689c1437e99cca0856dbc9c1
stephen-nene <stephen.nene@student.moringaschool.com>
2023-03-27 14:39:52 +0300

    added the front end link as trusted domain

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     user = User.where(sql, { username: user_params[:username], email: user_params[:email] }).first
     if user&.authenticate(user_params[:password])
       save_user(user.id)
-      response.headers['Access-Control-Allow-Origin'] = 'http://localhost:4000'
+      response.headers['Access-Control-Allow-Origin'] = 'https://todo-front-peach-kappa.vercel.app'
       response.headers['Access-Control-Allow-Credentials'] = 'true'
       app_response(message: 'Login was successful', status: :ok, data: { user: user, session: session[:uid] })
     else
